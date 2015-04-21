50f7c3819a1f3d627ca483cc275997bb747a1a19
Hooligan-0 <gwen-github@agilack.fr>
2015-04-21 23:46:03 +0200

    = ADD: Unit Test for bootloader : "Hello World"

---
+++ sw-ut_hello-bl/main.c
@@ -0,0 +1,24 @@
+/**
+ * NetSerial - Unit test "Hello world" with bootloader
+ *
+ * Copyright (c) 2015 Saint-Genest Gwenael <gwen@agilack.fr>
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation. (See COPYING.GPL for details.)
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+#include "hardware.h"
+#include "main.h"
+#include "uart.h"
+
+int main(void)
+{
+	uart_puts("\r\nHello World !\r\n");
+
+	while(1);
+}
+/* EOF */
---
+++ sw-ut_hello-bl/main.h
@@ -0,0 +1,19 @@
+/**
+ * NetSerial - Unit test "Hello world" with bootloader
+ *
+ * Copyright (c) 2015 Saint-Genest Gwenael <gwen@agilack.fr>
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 2 as published by the Free Software
+ * Foundation. (See COPYING.GPL for details.)
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#endif
+/* EOF */
